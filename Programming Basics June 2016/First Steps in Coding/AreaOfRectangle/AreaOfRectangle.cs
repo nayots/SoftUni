607de22cd17b344d18eea9c7b89b6d463784432a
Stoyan Grigorov <hacker4eto@gmail.com>
2016-06-15 20:38:52 +0200

    Commit

---
+++ Programming Basics June 2016/First Steps in Coding/AreaOfRectangle/AreaOfRectangle.cs
@@ -0,0 +1,11 @@
+﻿using System;
+//Problem 7: AreaOfRectangle
+class AreaOfRectangle
+{
+    static void Main(string[] args)
+    {
+        decimal a = decimal.Parse(Console.ReadLine());
+        decimal b = decimal.Parse(Console.ReadLine());
+        Console.WriteLine(a * b);
+    }
+}
---
+++ Programming Basics June 2016/First Steps in Coding/SquareOfStars/SquareOfStars.cs
@@ -0,0 +1,29 @@
+﻿using System;
+//Problem 8: SquareOfStars
+class SquareOfStars
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int row = 0; row < n; row++)
+        {
+            for (int col = 0; col < n; col++)
+            {
+                if (row == 0 || row == n - 1)
+                {
+                    Console.Write('*');
+                }
+                else if (col == 0 || col == n - 1)
+                {
+                    Console.Write('*');
+                }
+                else
+                {
+                    Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
