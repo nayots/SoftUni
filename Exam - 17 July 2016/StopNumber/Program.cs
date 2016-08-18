acee2c18cfbd87bac554998d0e75ed949f20bfcd
Stoyan Grigorov <hacker4eto@gmail.com>
2016-08-18 09:46:33 +0300

    Revert "Revert "Exam 17 july 2016 Practice""
    This reverts commit 4dd1e0df85822190e3237b0ae96c062cae7c9305.

---
+++ Exam - 17 July 2016/StopNumber/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+//StopNumber
+class StopNumber
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int m = int.Parse(Console.ReadLine());
+        int s = int.Parse(Console.ReadLine());
+
+        for (int i = m; i >= n; i--)
+        {
+            if (i % 2 == 0 && i % 3 == 0)
+            {
+                if (i != s)
+                {
+                    Console.Write("{0} ", i);
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
