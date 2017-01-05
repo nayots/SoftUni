a035cc73bf872617bf9cb7801830513a8d90337b
Stoyan Grigorov <grigorov.stoyan@gmail.com>
2017-01-05 20:22:28 +0200

    Added more exam problems solutions

---
+++ Programming Fundamentals September 2016/Exam Prep Random Problems/Exam Problems/Array Modifier/ArrayModifier.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayModifier
+{
+    class ArrayModifier
+    {
+        static void Main(string[] args)
+        {
+            long[] numbers = Console.ReadLine().Split().Select(long.Parse).ToArray();
+
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                string[] comArgs = command.Split();
+
+                int firstIndex = 0;
+                int secIndex = 0;
+
+                switch (comArgs[0])
+                {
+                    case "swap":
+
+                        firstIndex = int.Parse(comArgs[1]);
+                        secIndex = int.Parse(comArgs[2]);
+
+                        numbers = Swap(numbers, firstIndex, secIndex);
+                        break;
+                    case "multiply":
+
+                        firstIndex = int.Parse(comArgs[1]);
+                        secIndex = int.Parse(comArgs[2]);
+
+                        numbers = Multiply(numbers, firstIndex, secIndex);
+                        break;
+                    case "decrease":
+                        numbers = Decrease(numbers);
+                        break;
+                }
+
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(string.Join(", ",numbers));
+        }
+
+        private static long[] Decrease(long[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] -= 1;
+            }
+
+            return numbers;
+        }
+
+        private static long[] Multiply(long[] numbers, int firstIndex, int secIndex)
+        {
+            numbers[firstIndex] *= numbers[secIndex];
+
+            return numbers;
+        }
+
+        private static long[] Swap(long[] numbers, int firstIndex, int secIndex)
+        {
+            long tempData = numbers[firstIndex];
+
+            numbers[firstIndex] = numbers[secIndex];
+
+            numbers[secIndex] = tempData;
+
+            return numbers;
+        }
+    }
+}
---
+++ Programming Fundamentals September 2016/Exam Prep Random Problems/Exam Problems/Pop_Aggregation/PopulationAggregation.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Pop_Aggregation
+{
+    public class PopulationAggregation
+    {
+        static void Main(string[] args)
+        {
+            string command = Console.ReadLine();
+
+            List<Country> countries = new List<Country>();
+
+            while (command != "stop")
+            {
+                string[] commandArgs = command.Split('\\');
+
+                string country = "";
+                string city = "";
+
+                if (char.IsUpper(commandArgs[0][0]))
+                {
+                    country = commandArgs[0];
+                    city = commandArgs[1];
+                }
+                else
+                {
+                    country = commandArgs[1];
+                    city = commandArgs[0];
+                }
+
+                long population = long.Parse(commandArgs[2]);
+
+                //    \@|\#|\$|\&|[0-9]    removes all the unwanted symbols when used with Regex.Replace
+
+                country = Regex.Replace(country, @"\@|\#|\$|\&|[0-9]", string.Empty);
+
+                city = Regex.Replace(city, @"\@|\#|\$|\&|[0-9]", string.Empty);
+
+
+                if (countries.Any(x => x.Name == country))
+                {
+                    int cIndx = countries.FindIndex(x => x.Name == country);
+
+                    City town = new City();
+                    town.Name = city;
+                    town.Population = population;
+
+                    countries[cIndx].Cities.Add(town);
+
+                    countries[cIndx].Cities
+                        .Where(x => x.Name == city)
+                        .Select(c => c.Population = population).ToList();
+                }
+                else
+                {
+                    Country newCountry = new Country();
+
+                    newCountry.Name = country;
+
+                    City town = new City();
+                    town.Name = city;
+                    town.Population = population;
+
+                    newCountry.Cities.Add(town);
+
+                    countries.Add(newCountry);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            List<City> allTowns = new List<City>();
+
+            foreach (var country in countries.OrderBy(x => x.Name))
+            {
+                Console.WriteLine($"{country.Name} -> {country.Cities.Count}");
+
+                List<City> townsToadd = new List<City>();
+
+                townsToadd = country.Cities.GroupBy(x => x.Name).Select(y => y.First()).ToList();// Groups all the towns by name , then takes the first entry from each group, then makes a list.
+
+                allTowns.AddRange(townsToadd);
+            }
+
+            foreach (var town in allTowns.OrderByDescending(x => x.Population).Take(3))
+            {
+                Console.WriteLine($"{town.Name} -> {town.Population}");
+            }
+
+        }
+
+    }
+
+    public class Country
+    {
+        public string Name { get; set; }
+        public List<City> Cities { get; set; } = new List<City>();
+    }
+
+    public class City
+    {
+        public string Name { get; set; }
+        public long Population { get; set; }
+    }
+}
